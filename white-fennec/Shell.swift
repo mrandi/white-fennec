e7403a5a00e2cb6850e2e46839b926f7e2f06953
Gregsen <gkneitschel@gmail.com>
2016-01-07 17:13:33 +0100

    #1.0.1 extracted shell commands into own class. Commands are now run in login shell

---
+++ white-fennec/Shell.swift
@@ -0,0 +1,52 @@
+//
+//  Shell.swift
+//  white-fennec
+//
+//  Created by Gregor Kneitschel on 07/01/16.
+//  Copyright © 2016 Michele Randi. All rights reserved.
+//
+
+import Foundation
+
+class Shell {
+    
+    let command = NSTask()
+    let pipe = NSPipe()
+    
+    init(){
+        /// python and locales on a mac
+        command.environment = ["LC_ALL":"en_US.utf-8","LANG":"en_US.utf-8"]
+        
+        command.standardOutput = pipe //TODO: What about stderr?
+        
+        /// use current user shell
+        command.launchPath = NSProcessInfo().environment["SHELL"] as String!
+        
+    }
+    
+    func run(runCommand: String) throws -> String{
+        /// use the users login shell
+        command.arguments = ["-c", "-l", runCommand]
+        
+        /// run the command
+        command.launch()
+        
+        /// fetch data from pipe
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        
+        let output = NSString(data: data, encoding: NSUTF8StringEncoding)! as String
+        NSLog("\(output)")
+
+        /// block till ready
+        command.waitUntilExit()
+
+        NSLog("finished wating")
+       
+        if command.terminationStatus != 0 {
+            throw ShellException.ShellError
+        }
+        
+        /// convert to string and return
+        return output
+    }
+}
